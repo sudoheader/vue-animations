6e304fc58832221039bf5d3e115c80ed170e99aa
Raymond Nazaryan <rayn2427@gmail.com>
2018-05-06 20:08:00 -0700

    Added fade transition

--- src/App.vue
+++ src/App.vue
@@ -26,18 +26,19 @@
 
 <style>
     .fade-enter {
-        
+        opacity: 0;
     }
 
     .fade-enter-active {
-
+        transition: opacity 1s;
     }
     
     .fade-leave {
-
+        /* opacity: 1; */
     }
 
     .fade-enter-active {
-
+        transition: opacity 1s;
+        opacity: 0;
     }
 </style>
